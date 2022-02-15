ee1036cf2bbed8b32538f0c7067c5b3eaf531365
MariliaRonssen <marilia.er@gmail.com>
2022-02-14 22:10:35 -0300

    encontro remoto 3

---
+++ css/js/script.js
@@ -0,0 +1,9 @@
+function mostrarPopup(){
+    window.alert("BEM-VINDOS!");
+}
+
+let email = document.getElementById("campo-email");
+
+function enviarEmail(){
+    let emailDigitado = email.value;
+}
---
+++ js/script.js
@@ -0,0 +1,3 @@
+function mostrarPopup(){
+    window.alert("BEM-VINDOS!")
+}
